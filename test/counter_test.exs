cd7450a0e7c1f12e7a563a0247adb623e97855fb
Yatender Singh <yatender.nitk@gmail.com>
2020-04-25 06:36:21 +0530

    Modified CounterTest to test `Counter` APIs

--- test/counter_test.exs
+++ test/counter_test.exs
@@ -1,4 +1,23 @@
 defmodule CounterTest do
   use ExUnit.Case
   doctest Counter
+
+  test "use counter via APIs" do
+    pid = Counter.start(0)
+    pid2 = Counter.start(5)
+
+    assert 0 == Counter.state(pid)
+
+    Counter.tick(pid)
+    Counter.tick(pid)
+
+    assert 2 == Counter.state(pid)
+
+    # Check second counter
+    assert 5 == Counter.state(pid2)
+    Counter.tick(pid2)
+    Counter.tick(pid2)
+
+    assert 7 == Counter.state(pid2)
+  end
 end
